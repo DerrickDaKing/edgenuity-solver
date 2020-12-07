9af4ee8fc2924746ca9499f9e6d938d5a477a710
Ethan <barclayethan@gmail.com>
2020-12-06 22:02:56 -0500

    initial upload

---
+++ main.py
@@ -0,0 +1,84 @@
+import os
+import time
+import parse
+from selenium import webdriver
+from selenium.webdriver.firefox.options import Options
+from googlesearch import search
+
+def searchGoogle(query):
+    for j in search(query, tld="co.in", num=1, stop=1, pause=2): 
+        return j
+
+clear = lambda: os.system('clear') # For linux
+# clear = lambda: os.system('cls') # For windows
+
+
+driver_path = '/usr/bin/geckodriver'
+brave_path = '/usr/bin/firefox'
+
+quizletOptions = Options()
+quizletOptions.headless = True
+
+browser = webdriver.Firefox(executable_path=driver_path)
+quizletDriver = webdriver.Firefox(options=quizletOptions, executable_path=driver_path)
+
+browser.get("https://auth.edgenuity.com/Login/Login/Student")        
+
+def findAnswer(question):
+    clear()
+    print('Loading...')
+    print('Scanned!')
+    question = question.strip('\n')
+    question = question.strip('\t')
+    if question.find('A.') != -1:
+        question = question.partition('\n')[0]
+    question = question[:-3]
+    # print('Question?')
+    # question = input()
+    # print(question)
+
+    # Search google for quizlet url
+    quizletUrl = searchGoogle(question)
+    print('Googled!')
+    # print(quizletUrl)
+
+    # Download and parse quizlet data
+    quizletDriver.get(quizletUrl)
+    print('Loaded quizlet!')
+    answerElement = quizletDriver.find_element_by_xpath("//span[contains(text(),'" + question + "')]/../../../..//a[@class='SetPageTerm-definitionText']/span")
+    print('Parsed!')
+    answer = answerElement.text
+    clear()
+    print(answer)
+
+    # Prepare for next question
+    # quizletDriver.quit()
+
+def scan():
+    ready = True
+    lastQuestion = ''
+    while True:
+        if ready:
+            # Parse out question text
+            iframe = browser.find_element_by_id('stageFrame')
+            browser.switch_to_frame(iframe)
+            questions = browser.find_elements_by_class_name('Practice_Question_Body')
+            for i in range(len(questions)):
+                if questions[i].text != '':
+                    if lastQuestion != questions[i].text:
+                        lastQuestion = questions[i].text
+                        findAnswer(questions[i].text)
+                        break
+                    else:
+                        ready = False
+                        break
+            browser.switch_to.default_content()
+        else:
+            ready = True
+            time.sleep(1)       
+
+while True:
+    print('?')
+    x = input()
+    if x == 'run':
+        scan()
